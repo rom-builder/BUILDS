f9fdb58f75da28edf3f50db0933ec73308b567df
Mohammed Rabil <63334479+rabilrbl@users.noreply.github.com>
2023-02-14 08:46:54 +0000

    add jq to deps

--- util.sh
+++ util.sh
@@ -3,7 +3,7 @@
 prefix="device"
 
 resolve_dependencies() {
-  sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip openssl libssl-dev fontconfig $@ -y
+  sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip openssl libssl-dev fontconfig jq $@ -y
 }
 
 git_setup() {
