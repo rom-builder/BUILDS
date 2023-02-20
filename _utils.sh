51202837ee29bd4c06de6effc22f531c86fc1e9b
Mohammed Rabil <rabil@techie.com>
2023-02-20 21:00:46 +0530

    Add `open-jdk-8' dependency

--- _utils.sh
+++ _utils.sh
@@ -60,7 +60,7 @@ logt() {
 }
 
 resolve_dependencies() {
-  local packages=('repo' 'git-core' 'gnupg' 'flex' 'bison' 'build-essential' 'zip' 'curl' 'zlib1g-dev' 'libc6-dev-i386' 'libncurses5' 'lib32ncurses5-dev' 'x11proto-core-dev' 'libx11-dev' 'lib32z1-dev' 'libgl1-mesa-dev' 'libxml2-utils' 'xsltproc' 'unzip' 'openssl' 'libssl-dev' 'fontconfig' 'jq')
+  local packages=('repo' 'git-core' 'gnupg' 'flex' 'bison' 'build-essential' 'zip' 'curl' 'zlib1g-dev' 'libc6-dev-i386' 'libncurses5' 'lib32ncurses5-dev' 'x11proto-core-dev' 'libx11-dev' 'lib32z1-dev' 'libgl1-mesa-dev' 'libxml2-utils' 'xsltproc' 'unzip' 'openssl' 'libssl-dev' 'fontconfig' 'jq' 'openjdk-8-jdk')
   echo "Updating package lists..."
   sudo apt-get update -y 
   echo "Installing dependencies..."
